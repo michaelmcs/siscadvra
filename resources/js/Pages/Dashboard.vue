9fbccfa6cc73a157ba910e30b6870d4f1b60e971
brayan2903 <71749066+brayan2903@users.noreply.github.com>
2024-06-09 22:18:02 -0500

    Primer push carga academica

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            Dashboard
+        </template>
+
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 border-b border-gray-200">
+                You're logged in!123
+            </div>
+        </div>
+        <a-button type="primary">Primary Button</a-button>
+    <a-button>Default Button</a-button>
+    </AuthenticatedLayout>
+</template>
+
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
+import { Head } from '@inertiajs/vue3';
+import {ref} from 'vue';
+</script>
